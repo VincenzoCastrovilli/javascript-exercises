39ef9f1c67afb24ce2a40dd6b4efe6ade97c937c
VincenzoCastrovilli <vcastrovilli@icloud.com>
2023-01-31 11:05:25 +0100

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  newArray = [];
+  newArray = array.filter(elem => !args.includes(elem));
+  return newArray;
 };
 
 // Do not edit below this line
