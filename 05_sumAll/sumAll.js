f45e144f4b28d1ea9e1eb6465540d9f2c464f0fc
VincenzoCastrovilli <vcastrovilli@icloud.com>
2023-01-31 11:30:54 +0100

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0) {
+    return 'ERROR';
+  }
 
+  if ((typeof a != 'number') || (typeof b != 'number')) {
+    return 'ERROR';
+  }
+
+  if (b < a) {
+    let temp = b;
+    b = a;
+    a = temp;
+  }
+
+  let sum = 0;
+  for (i = 0; i < (b-a+1); i++) {
+    sum += (a + i);
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
