82f19ad33d21fc613ba05b31583599870e66ae08
VincenzoCastrovilli <vcastrovilli@icloud.com>
2023-01-31 12:17:39 +0100

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {  
+  let result = (fTemp - 32) * (5/9);
+  return Math.round(result * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let result = (cTemp * (9/5) + 32);
+  return Math.round(result*10)/10;
 };
 
 // Do not edit below this line
