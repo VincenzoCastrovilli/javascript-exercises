769567a78a41b8fa795e4b1e4a642e84b32bce37
VincenzoCastrovilli <vcastrovilli@icloud.com>
2023-02-03 18:45:24 +0100

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const firstString = string.toLowerCase().replace(/[^a-z]/g, "");
+  const lastString = firstString.split('').reverse().join('');
+  return firstString === lastString;
 };
 
 // Do not edit below this line
