3bae9c2322001e61c6b3ae4c2ae5b6954943d941
VincenzoCastrovilli <vcastrovilli@icloud.com>
2023-02-03 18:32:49 +0100

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	return array.reduce((acc, value) => acc + value, sum)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let initialValue = 1;
+	return array.reduce((acc, value) => acc * value, initialValue)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  let fact = 1;
+  for (i=a; i>0; i--) {
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
